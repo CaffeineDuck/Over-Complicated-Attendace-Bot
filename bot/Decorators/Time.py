bc739044b217ad19451adce61fd71cb59d3cd01a
Samrid Pandit <samrid@pop-os.localdomain>
2021-04-23 10:46:39 +0545

    Made the wait time customizable by the user

--- bot/Decorators/Time.py
+++ bot/Decorators/Time.py
@@ -1,7 +1,8 @@
 import time
 
+from config.bot import bot_config
 
-def wait(secs: int = 2):
+def wait(secs: int = bot_config.slow_internet):
     def predicate(func):
         def inner(*args, **kwargs):
             func(*args, **kwargs)
