196d4d0a86ac61793c5c573567d122a258ef06e5
Samrid Pandit <samrid@pop-os.localdomain>
2021-04-23 16:06:59 +0545

    Provided default values of xpath

--- bot/DataClass/School.py
+++ bot/DataClass/School.py
@@ -6,12 +6,14 @@ class School:
     start_time: int
     end_time: int
     class_interval: int
-    school_site: str
-    username_xpath: str
-    password_xpath: str
-    login_button_xpath: str
-    notification_cancel_button_xpath: str
-    online_class_xpath: str
-    join_button_xpath: str
-    profile_button_xpath: str
-    logout_button_xpath: str
+    school_site: str = "https://ingrails.com/school"
+    username_xpath: str = "/html/body/section/div/section[1]/div/form/input[2]"
+    password_xpath: str = "/html/body/section/div/section[1]/div/form/input[3]"
+    login_button_xpath: str = "/html/body/section/div/section[1]/div/form/button"
+    notification_cancel_button_xpath: str = "/html/body/div[7]/div/div/div[3]/button"
+    online_class_xpath: str = '//*[@id="onlineClassMenu"]'
+    join_button_xpath: str = (
+        '//*[@id="right-side"]/section/div/div/div[1]/section/ul/li/span/a[1]'
+    )
+    profile_button_xpath: str = "/html/body/header/nav/div/ul/li/a"
+    logout_button_xpath: str = "/html/body/header/nav/div/ul/li/ul/li/div/a"
--- bot/__main__.py
+++ bot/__main__.py
@@ -11,15 +11,6 @@ school = School(
     start_time=class_config.start,
     end_time=class_config.end,
     class_interval=class_config.interval,
-    school_site="https://ingrails.com/school",
-    username_xpath="/html/body/section/div/section[1]/div/form/input[2]",
-    password_xpath="/html/body/section/div/section[1]/div/form/input[3]",
-    login_button_xpath="/html/body/section/div/section[1]/div/form/button",
-    notification_cancel_button_xpath="/html/body/div[7]/div/div/div[3]/button",
-    online_class_xpath='//*[@id="onlineClassMenu"]',
-    join_button_xpath='//*[@id="right-side"]/section/div/div/div[1]/section/ul/li/span/a[1]',
-    profile_button_xpath="/html/body/header/nav/div/ul/li/a",
-    logout_button_xpath="/html/body/header/nav/div/ul/li/ul/li/div/a",
 )
 
 main_bot = AttendanceBot(
